ca37969aa5911acea49ff5ac508b9874938fa0a8
ShaliniAvindya <132177735+ShaliniAvindya@users.noreply.github.com>
2025-09-14 00:54:35 +0530

    Update Blog.js

--- Server/models/Blog.js
+++ Server/models/Blog.js
@@ -3,7 +3,12 @@ const mongoose = require('mongoose');
 const BlogContentSchema = new mongoose.Schema({
   heading: String,
   image: String,
-  text: String
+  text: String,
+  blockType: String,
+  blockData: {
+    type: mongoose.Schema.Types.Mixed,
+    default: {}
+  }
 }, { _id: false });
 
 const BlogSchema = new mongoose.Schema({
@@ -23,7 +28,20 @@ const BlogSchema = new mongoose.Schema({
   images: [String],
   videos: [String],
   tags: [String],
-  author: String
+  author: String,
+  theme: {
+    type: mongoose.Schema.Types.Mixed,
+    default: {
+      name: 'Default',
+      primaryColor: '#074a5b',
+      secondaryColor: '#3B82F6',
+      accentColor: '#F97316',
+      backgroundColor: '#FFFFFF',
+      textColor: '#1F2937',
+      borderRadius: '8px',
+      fontFamily: '"Comic Sans MS", "Comic Neue"'
+    }
+  }
 }, { timestamps: true });
 
 module.exports = mongoose.model('Blog', BlogSchema);
