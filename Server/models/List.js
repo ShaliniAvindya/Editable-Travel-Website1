e660f3522ab535c02716f2a94d0cf77d57275a63
ShaliniAvindya <132177735+ShaliniAvindya@users.noreply.github.com>
2025-10-12 14:37:11 +0530

    Create List.js

---
+++ Server/models/List.js
@@ -0,0 +1,11 @@
+const mongoose = require('mongoose');
+
+const ListSchema = new mongoose.Schema({
+  name: { type: String, required: true },
+  description: { type: String },
+  is_public: { type: Boolean, default: false },
+  subscribers: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Newsletter' }],
+  created_by: { type: String },
+}, { timestamps: true });
+
+module.exports = mongoose.model('List', ListSchema);
