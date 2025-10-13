42eb989e96558b330dae3c6e3aea2735c5bbd442
ShaliniAvindya <132177735+ShaliniAvindya@users.noreply.github.com>
2025-10-13 15:31:50 +0530

    Update List.js

--- Server/models/List.js
+++ Server/models/List.js
@@ -4,6 +4,7 @@ const ListSchema = new mongoose.Schema({
   name: { type: String, required: true },
   description: { type: String },
   is_public: { type: Boolean, default: false },
+  language: { type: String, default: '' },
   subscribers: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Newsletter' }],
   created_by: { type: String },
 }, { timestamps: true });
